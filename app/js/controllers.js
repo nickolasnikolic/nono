d28d9df829f5c29753ce84058a68ff1de3026c50
Nickolas Nikolic <subclass@gmail.com>
2015-08-16 15:16:10 -0500

    inching towards itinerary

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -643,6 +643,9 @@ nonoApp.controller('ItineraryController', ['$scope', 'LoverRegistryService', fun
       })
       .then(function(){
         _.each($scope.dates,function(date){
+
+          console.log(date);
+
           $.getJSON('../api/itinerary/messages/' + date.romantic_date_id)
               .success(function(data){
                 console.log(data);
