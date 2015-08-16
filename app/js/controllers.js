2c6df07cafeb94998ea3c2e37f77d1878911048f
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-08-15 23:22:36 -0500

    testing

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -364,7 +364,7 @@ nonoApp.controller('SchedulingController', ['$scope', '$stateParams', 'LoverRegi
         console.log('data is pulled for scheduling from the current lover, and it is: ');
         console.log(data);
 
-        askerDatableDaysString = data.datableDays[0].datable_days;
+        askerDatableDaysString = JSON.parse(data.datableDays[0].datable_days);
         askerEventsHolder.concat( data.datesAsked );
         askerEventsHolder.concat( data.datesGiven );
       })
