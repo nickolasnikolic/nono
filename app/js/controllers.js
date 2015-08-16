4e99d327aea0f23ef3d56c357dd2dbda25a5da14
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-08-16 01:45:57 -0500

    calandar display happening, still needs tuning

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -404,7 +404,7 @@ nonoApp.controller('SchedulingController', ['$scope', '$stateParams', 'LoverRegi
                   'asker': element.asker,
                   'giver': element.giver,
                   'id': element.romantic_date_id,
-                  'title': 'a date,
+                  'title': 'set date',
                   'start': element.date_start,
                   'end': element.date_end //todo add date end to db entry for here
                 } );
