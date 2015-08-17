1cdd5f3399383253d48c02ee995798e247f512c4
Nickolas Nikolic <subclass@gmail.com>
2015-08-17 10:49:41 -0500

    inching towards itinerary values

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -608,7 +608,7 @@ nonoApp.controller('SchedulingController', ['$scope', '$state', '$stateParams', 
 
                   suppository.asker = LoverRegistryService.userId;
                   suppository.giver = $stateParams.loveInterest;
-                  suppository.date = moment(date.d).format('YYYY-MM-DD HH:hh:ss');
+                  suppository.date = date.format('YYYY-MM-DD HH:hh:ss');
 
                   LoverRegistryService.userDate = suppository;
 
