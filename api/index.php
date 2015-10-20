8ecd37a75c93d32236576dd7cd6ea5ac89aec592
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-10-20 15:26:50 -0500

    increased drop limit in db

--- api/index.php
+++ api/index.php
@@ -177,7 +177,7 @@ $app->get('/selection/:notthisone', function($notthisone){
 
   $db = new PDO( "mysql:host=$server;dbname=$dbname;charset=utf8", $username, $password);
   $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  $stmt = $db->prepare('SELECT * FROM lovers WHERE user_id != :notthisone AND frozen = 0 LIMIT 500');
+  $stmt = $db->prepare('SELECT * FROM lovers WHERE user_id != :notthisone AND frozen = 0 LIMIT 3000');
   $stmt->bindParam(':notthisone', $notthisone);
   $stmt->execute();
   $result = $stmt->fetchAll();
